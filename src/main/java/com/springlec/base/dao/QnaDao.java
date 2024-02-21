74841b0c05b6a1ca85819aa5102a256d5844748c
tom3017 <tom3017@naver.com>
2024-02-21 20:57:36 +0900

    2024-02-21 21:00 FAQ 페이지 보여주기  Q&A 페이지 보여주기 구현완료 Q&A DB 데이터 불러오기 구현 예정중
    2024-02-21 21:00 FAQ 페이지 보여주기  Q&A 페이지 보여주기 구현완료 Q&A DB 데이터 불러오기 구현 예정중

---
+++ src/main/java/com/springlec/base/dao/QnaDao.java
@@ -0,0 +1,11 @@
+package com.springlec.base.dao;
+
+import java.util.List;
+
+import com.springlec.base.model.QnaDto;
+
+public interface QnaDao {
+	public List<QnaDto> listDao() throws Exception;
+	
+	
+}
---
+++ src/main/java/com/springlec/base/service/QnaDaoService.java
@@ -0,0 +1,9 @@
+package com.springlec.base.service;
+
+import java.util.List;
+
+import com.springlec.base.model.QnaDto;
+
+public interface QnaDaoService {
+	public List<QnaDto> listDao() throws Exception;
+}
