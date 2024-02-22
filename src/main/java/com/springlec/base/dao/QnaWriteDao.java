f7cf2e4a22429a0db790b6e252a0ace1896011f0
tom3017 <tom3017@naver.com>
2024-02-22 20:35:08 +0900

    2024-02-22 20:34 Q&A 글작성 시 아이디 세션 가지고오는 작업 진행중
    2024-02-22 20:34 Q&A 글작성 시 아이디 세션 가지고오는 작업 진행중

--- src/main/java/com/springlec/base/dao/QnaWriteDao.java
+++ src/main/java/com/springlec/base/dao/QnaWriteDao.java
@@ -1,5 +1,9 @@
 package com.springlec.base.dao;
 
+import org.springframework.web.multipart.MultipartFile;
+
 public interface QnaWriteDao {
-	public void writeDao(String qnaTitle, String qnaCategory, String qnaContent, String qnaImage, String qnaDate) throws Exception;
+	 public void writeDao(String qnaTitle, String qnaCategory, String qnaContent, String qnaImage, String userId) throws Exception;
+	public String uploadFile(MultipartFile file) throws Exception;
+
 }
--- src/main/java/com/springlec/base/service/QnaWriteService.java
+++ src/main/java/com/springlec/base/service/QnaWriteService.java
@@ -1,5 +1,8 @@
 package com.springlec.base.service;
 
+import org.springframework.web.multipart.MultipartFile;
+
 public interface QnaWriteService {
-	public void writeDao(String qnaTitle, String qnaCategory, String qnaContent, String qnaImage, String qnaDate) throws Exception;
+	 public void writeDao(String qnaTitle, String qnaCategory, String qnaContent, String qnaImage, String userId) throws Exception;
+	public String uploadFile(MultipartFile file) throws Exception;
 }
