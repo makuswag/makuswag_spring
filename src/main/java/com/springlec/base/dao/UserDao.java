372d56e6b4fff95c633ee843a205470e737b12c3
DaegeunLee <foejakzm@naver.com>
2024-02-28 20:46:56 +0900

    2024-02-28 회원가입 commit

--- src/main/java/com/springlec/base/dao/UserDao.java
+++ src/main/java/com/springlec/base/dao/UserDao.java
@@ -6,9 +6,12 @@ public interface UserDao {
 
 	public UserDto whologin(String userId, String userPasswd) throws Exception;
 	public void changePasswd(String userPasswd, String userId) throws Exception;
+	public void sign(String userId, String userPw, String name, String postcode, String address, String phone, String email, String gender, String birthday) throws Exception;
 	public UserDto findId(String name, String email) throws Exception;
 	public UserDto findPw(String userId, String name, String email) throws Exception;
 	
 	public Integer checkDuplicatedId(String userId) throws Exception;
 	public Integer checkDuplicatedEmail(String email) throws Exception;
+	
+	
 }
--- src/main/webapp/WEB-INF/views/status/adminSuccess.jsp
+++ src/main/webapp/WEB-INF/views/status/adminSuccess.jsp
@@ -1,6 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
-<%@page import="com.springlec.base.model.UserDto"%>%>
+<%@page import="com.springlec.base.model.UserDto"%>
 <%
     Object userObject = session.getAttribute("user");
 
