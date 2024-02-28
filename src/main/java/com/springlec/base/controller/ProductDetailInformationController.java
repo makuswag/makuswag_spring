52ca87ebfd55de53879af69868a7a94d6360322b
zlzlwn <zlzlwn1018@gmail.com>
2024-02-29 00:09:46 +0900

    2024-02-29 00:09 상품 구매 중간 commit
    2024-02-29 00:09 상품 구매 중간 commit

--- src/main/java/com/springlec/base/controller/ProductDetailInformationController.java
+++ src/main/java/com/springlec/base/controller/ProductDetailInformationController.java
@@ -25,22 +25,22 @@ public class ProductDetailInformationController {
 	//제품 상세페이지 화면 보여주기(이미지,상세내용), @RequestParam("proName") String proName 삭제,detail(proName)삭제(정민이형 끝나면 추가하기)
 	@GetMapping("productDetailInformation")
 	public String detail(@RequestParam(name = "proName", required = false) String proName,HttpServletRequest request, Model model) throws Exception{
-		ProductDetailInformationDto productDetailInformation = service.productdetail(proName); // bId에 해당하는 게시물 정보를 가져옴(사진이랑 상품정보 다 가져옴)
+		ProductDetailInformationDto productDetailInformationDto = service.productdetail(proName); // bId에 해당하는 게시물 정보를 가져옴(사진이랑 상품정보 다 가져옴)
 		 // 오류확인(나중에 지우
-	    if (productDetailInformation == null) {
+	    if (productDetailInformationDto == null) {
 	        System.out.println("productDetailInformation is null");
 	    } else {
-	        System.out.println("productDetailInformation: " + productDetailInformation.toString());
+	        System.out.println("productDetailInformation: " + productDetailInformationDto.toString());
 	    }
 	    //여기까지
-		model.addAttribute("productDetailInformation", productDetailInformation); 
+		model.addAttribute("productDetailInformation", productDetailInformationDto); 
 		 // proSeq 세션에 저장
 	    HttpSession session = request.getSession();
-	    session.setAttribute("proSeq", productDetailInformation.getProSeq());
+	    session.setAttribute("proSeq", productDetailInformationDto.getProSeq());
 		List<String> colors = service.color(proName);
 		model.addAttribute("colors", colors);
 		System.out.println("받아온 상품이름"+proName);
-		System.out.println("제품정보: "+productDetailInformation);
+		System.out.println("제품정보: "+productDetailInformationDto);
 		System.out.println("색 정보: "+colors);
 //		List<String> colors = service.color(proName);
 //		model.addAttribute("colors", colors);
@@ -64,14 +64,12 @@ public class ProductDetailInformationController {
 	    }
 		String userId = user.getUserId();
 		
-		System.out.println("유저아이디"+userId);
+		System.out.println("유저아이디productPurchase"+userId);
 		
-		String revContent = request.getParameter("reviewContent");
-//		service.
 		
 		//세션정의: 어디서 꺼야하는지 다시 알아보기(여기서 끄면 상품 등록이 안됨
 //		sessionProductSeq.invalidate(); 
-		return "redirect:/productDetailInformation";//메인으로 보내야함?
+		return "redirect:/myPage";//메인으로 보내야함?
 		
 	}
 	
